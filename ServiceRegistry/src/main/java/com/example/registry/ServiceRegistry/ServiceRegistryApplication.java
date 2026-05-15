50a38ac664c8d15b65fb672ae9169ec673b385e2
AAMIR-DELL\aamir <aamirsayed44@gmail.com>
2026-05-15 11:30:46 +0530

    added blank service registry as maven project

---
+++ ServiceRegistry/src/main/java/com/example/registry/ServiceRegistry/ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.registry.ServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
